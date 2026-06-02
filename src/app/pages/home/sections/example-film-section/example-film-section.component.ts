8258894b57ce5cadc16bc607abadd5cacf768e29
Lucastepp <lucastep.uk@gmail.com>
2026-06-02 15:53:40 +0100

    Add example film and refine Blue Lagoon footer

---
+++ src/app/pages/home/sections/example-film-section/example-film-section.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example-film-section',
+  templateUrl: './example-film-section.component.html',
+})
+export class ExampleFilmSectionComponent {}
